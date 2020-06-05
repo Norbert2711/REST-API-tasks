746dc9f8a51c71ed56c10e9bdf162ecc23d26142
Norbert2711 <norbertu2711@gmail.com>
2020-06-05 21:45:54 +0200

    REST finished

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,20 +1,19 @@
 package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class TasksApplication  extends SpringBootServletInitializer{
+@SpringBootApplication
 
+public class TasksApplication {
+    //extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
 
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-        return application.sources(TasksApplication.class);
-    }
-
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(TasksApplication.class);
+//    }
 }
