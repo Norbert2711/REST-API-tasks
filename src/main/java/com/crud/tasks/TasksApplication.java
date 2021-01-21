91c626da16086b82cf86afabaa6c349622220e47
Norbert2711 <norbertu2711@gmail.com>
2021-01-21 18:42:13 +0100

    first

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,14 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class TasksApplication {
-    //extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(TasksApplication.class);
-//    }
+
 }
